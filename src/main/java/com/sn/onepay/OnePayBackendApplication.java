c51a676f4999fe52902976c74f0d002a3d85b998
Mouhamed <mac@192.168.1.8>
2024-10-18 21:32:58 +0000

    Init project

---
+++ src/main/java/com/sn/onepay/OnePayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sn.onepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnePayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnePayBackendApplication.class, args);
+	}
+
+}
